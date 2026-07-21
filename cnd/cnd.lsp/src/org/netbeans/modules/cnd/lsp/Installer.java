f0707cf065d3f4067b590a36311d45ff7375c0bd
Piotr Hoppe <piotrhoppe@users.noreply.github.com>
2026-07-21 22:50:28 +0200

    feat: add Yenta module installer to cnd.lsp
    - Add Yenta API v1.1 dependency with Maven unpack configuration
    - Create Installer class extending Yenta for module initialization

---
+++ cnd/cnd.lsp/src/org/netbeans/modules/cnd/lsp/Installer.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2026 Piotr Hoppe <piotr.hoppe.00@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.netbeans.modules.cnd.lsp;
+
+import java.util.Collections;
+import java.util.Set;
+import org.netbeans.contrib.yenta.Yenta;
+
+public class Installer extends Yenta {
+    @Override
+    protected Set<String> friends() {
+        return Collections.singleton("org.netbeans.modules.dlight.nativeexecution");
+    }
+}
